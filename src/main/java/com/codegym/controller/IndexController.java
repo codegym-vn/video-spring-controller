b5798ef330c264d449b4c1d0917376e238c44233
nguyenkhanhtung <tungnk0711@gmail.com>
2020-05-19 17:00:54 +0700

    cm

---
+++ src/main/java/com/codegym/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class IndexController {
+
+    @RequestMapping("/")
+    public String get(Model model){
+        //mapped to hostname:port/home/
+        model.addAttribute("message", "Home!");
+        return "/home";
+    }
+
+    @RequestMapping("/index")
+    public String index(Model model){
+        //mapped to hostname:port/home/index/
+        model.addAttribute("message", "Index!");
+        return "/index";
+    }
+
+}
